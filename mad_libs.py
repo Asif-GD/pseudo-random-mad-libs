f6dad47a40a26a06e222349b1f641a7650bf054b
Asif-GD <asif.coach@gmail.com>
2022-05-10 20:01:15 +0530

    created the get_user_inputs() function; - reordered the inputs for better readability

--- mad_libs.py
+++ mad_libs.py
@@ -8,43 +8,43 @@
 # sample_mad_lib = f"Hi {name}, you are here to {verb_1}. Do not give up for you are {adjective_1}."
 # print(sample_mad_lib)
 
+def get_user_input():
+    noun_1 = input("a noun (animal): ")
+    noun_2 = input("another noun (place/location): ")
+    noun_3 = input("and another noun (food): ")
+    noun_4 = input("and yet another noun (an animal, last one, I promise!): ")
+    verb_1 = input("a verb (past tense): ")
+    verb_2 = input("another verb: ")
+    adjective_1 = input("an adjective: ")
+    adjective_2 = input("another adjective: ")
+    adjective_3 = input("another adjective: ")
+    adjective_4 = input("and another adjective: ")
+
+    print("\n and we are done. Enjoy your story.")
+
+    user_inputs = {
+        "noun_1": noun_1,
+        "noun_2": noun_2,
+        "noun_3": noun_3,
+        "noun_4": noun_4,
+        "verb_1": verb_1,
+        "verb_2": verb_2,
+        "adjective_1": adjective_1,
+        "adjective_2": adjective_2,
+        "adjective_3": adjective_3,
+        "adjective_4": adjective_4,
+
+    }
+    return user_inputs
+
+
+inputs = get_user_input()
 # random_story = randint(1, 5)
-
 random_story = 1
-
-adjective_1 = input("an adjective: ")
-noun_1 = input("a noun (animal): ")
-verb_1 = input("a verb (past tense): ")
-# adverb_1 = input("an adverb: ")
-adjective_2 = input("an adjective: ")
-noun_2 = input("a noun (place/location): ")
-noun_3 = input("a noun (food): ")
-noun_4 = input("a noun (animal): ")
-adjective_3 = input("an adjective: ")
-verb_2 = input("a verb: ")
-# adverb_2 = input("an adverb: ")
-# verb_3 = input("a verb (past tense): ")
-adjective_4 = input("an adjective: ")
-
-user_inputs = {
-    "adjective_1": adjective_1,
-    "noun_1": noun_1,
-    "verb_1": verb_1,
-    # "adverb_1": adverb_1,
-    "adjective_2": adjective_2,
-    "noun_2": noun_2,
-    "noun_3": noun_3,
-    "noun_4": noun_4,
-    "adjective_3": adjective_3,
-    "verb_2": verb_2,
-    # "adverb_2": adverb_2,
-    # "verb_3": verb_3,
-    "adjective_4": adjective_4,
-}
 
 if random_story == 1:
     file = open("01_mad_libs.txt", "r")
     mad_libs = file.read()
     file.close()
 
-print(mad_libs.format(**user_inputs))
+print(mad_libs.format(**inputs))
