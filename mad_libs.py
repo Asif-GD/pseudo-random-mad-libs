ae3a9dd344756cd4024b0f3fd6541ebdddf275d6
Asif-GD <asif.coach@gmail.com>
2022-05-10 17:06:00 +0530

    first commit, committing the default files of the project

---
+++ mad_libs.py
@@ -0,0 +1,31 @@
+# mad libs
+# I will export a random mab lib from a file and ask the user to input words for it.
+# name = input("May I know your name please? ")
+# verb_1 = input("a verb: ")
+# adjective_1 = input("an adjective: ")
+#
+# sample_mad_lib = f"Hi {name}, you are here to {verb_1}. Do not give up for you are {adjective_1}."
+# print(sample_mad_lib)
+
+adjective_1 = input("an adjective: ")
+noun_1 = input("a noun (animal): ")
+verb_1 = input("a verb (past tense): ")
+adverb_1 = input("an adverb: ")
+adjective_2 = input("an adjective: ")
+noun_2 = input("a noun (place/location): ")
+noun_3 = input("a noun (food): ")
+noun_4 = input("a noun (animal): ")
+adjective_3 = input("an adjective: ")
+verb_2 = input("a verb: ")
+adverb_2 = input("an adverb: ")
+verb_3 = input("a verb (past tense): ")
+adjective_4 = input("an adjective: ")
+
+mad_libs = f"A Day At The Zoo! \n \
+Today I went to the zoo. I saw a(n) {adjective_1} {noun_1} jumping up and down in its habitat. \n \
+He {verb_1} {adverb_1} through the large tunnel that led to its {adjective_2} {noun_2}. \n \
+I got some {noun_3} and passed them through the cage to a gigantic gray {noun_4} towering above my head. \n \
+Feeding that animal made me hungry. I went to get a {adjective_3} scoop of ice cream. It filled my stomach. \n \
+Afterwards I had to {verb_2} {adverb_2} to catch our bus. When I got home I {verb_3} my \n \
+mom for a {adjective_4} day at the zoo."
+print(mad_libs)
