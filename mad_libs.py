0b3103936b6347558264a4105e14445ff2a916d9
Asif-GD <asif.coach@gmail.com>
2022-05-11 16:04:19 +0530

    - added the second story "A Day At The Amusement Park" - added one more user input {verb_3} - rewrote stories 1 and 2 to align with the changes of the users' input - implemented the random-number-generator using 'randint'

--- mad_libs.py
+++ mad_libs.py
@@ -8,19 +8,23 @@
 # sample_mad_lib = f"Hi {name}, you are here to {verb_1}. Do not give up for you are {adjective_1}."
 # print(sample_mad_lib)
 
+from random import randint
+
+
 def get_user_input():
-    noun_1 = input("a noun (animal): ")
+    noun_1 = input("a noun (plural): ")
     noun_2 = input("another noun (place/location): ")
     noun_3 = input("and another noun (food): ")
-    noun_4 = input("and yet another noun (an animal, last one, I promise!): ")
+    noun_4 = input("and yet another noun (last one, I promise!): ")
     verb_1 = input("a verb (past tense): ")
-    verb_2 = input("another verb: ")
-    adjective_1 = input("an adjective: ")
+    verb_2 = input("another verb (past tense): ")
+    verb_3 = input("and another verb (ed!): ")
+    adjective_1 = input("an adjective (color): ")
     adjective_2 = input("another adjective: ")
-    adjective_3 = input("another adjective: ")
+    adjective_3 = input("another adjective (superlative): ")
     adjective_4 = input("and another adjective: ")
 
-    print("\n and we are done. Enjoy your story.")
+    print("...and we are done. I hope you enjoy your story. \n")
 
     user_inputs = {
         "noun_1": noun_1,
@@ -29,6 +33,7 @@ def get_user_input():
         "noun_4": noun_4,
         "verb_1": verb_1,
         "verb_2": verb_2,
+        "verb_3": verb_3,
         "adjective_1": adjective_1,
         "adjective_2": adjective_2,
         "adjective_3": adjective_3,
@@ -38,13 +43,18 @@ def get_user_input():
     return user_inputs
 
 
-inputs = get_user_input()
-# random_story = randint(1, 5)
-random_story = 1
+random_story = randint(1, 2)
 
 if random_story == 1:
+    print("A Day At The Zoo!")
     file = open("01_mad_libs.txt", "r")
     mad_libs = file.read()
     file.close()
+elif random_story == 2:
+    print("A Day At The Amusement Park!")
+    file = open("02_mad_libs.txt", "r")
+    mad_libs = file.read()
+    file.close()
 
+inputs = get_user_input()
 print(mad_libs.format(**inputs))
