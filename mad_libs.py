f0f338082d9d7df85843006b071fc06733b64365
Asif-GD <asif.coach@gmail.com>
2022-05-12 11:34:50 +0530

    - added a new story 'First Day At School.' - added additional words to the list of user_inputs 'noun_5', 'verb_4' - rewrote the files '01_mad_libs.txt' & '02_mad_libs.txt'

--- mad_libs.py
+++ mad_libs.py
@@ -5,17 +5,19 @@ from random import randint
 
 
 def get_user_input():
-    noun_1 = input("a noun (plural): ")
-    noun_2 = input("another noun (place/location): ")
-    noun_3 = input("and another noun (food): ")
-    noun_4 = input("and yet another noun (last one, I promise!): ")
-    verb_1 = input("a verb (past tense): ")
-    verb_2 = input("another verb (past tense): ")
-    verb_3 = input("and another verb (ed!): ")
-    adjective_1 = input("an adjective (color): ")
-    adjective_2 = input("another adjective: ")
-    adjective_3 = input("another adjective (superlative): ")
-    adjective_4 = input("and another adjective: ")
+    noun_1 = input("a noun (plural): ").upper()
+    noun_2 = input("another noun (place/location): ").upper()
+    noun_3 = input("and another noun (food): ").upper()
+    noun_4 = input("and yet another noun (a proper noun): ").upper()
+    noun_5 = input("and another noun (animal): ").upper()
+    verb_1 = input("a verb (past tense): ").upper()
+    verb_2 = input("another verb (past tense): ").upper()
+    verb_3 = input("and another verb (ed!): ").upper()
+    verb_4 = input("and another verb (-ing): ").upper()
+    adjective_1 = input("an adjective (color): ").upper()
+    adjective_2 = input("another adjective: ").upper()
+    adjective_3 = input("another adjective (superlative): ").upper()
+    adjective_4 = input("and another adjective: ").upper()
 
     print("...and we are done. I hope you enjoy your story. \n")
 
@@ -24,9 +26,11 @@ def get_user_input():
         "noun_2": noun_2,
         "noun_3": noun_3,
         "noun_4": noun_4,
+        "noun_5": noun_5,
         "verb_1": verb_1,
         "verb_2": verb_2,
         "verb_3": verb_3,
+        "verb_4": verb_4,
         "adjective_1": adjective_1,
         "adjective_2": adjective_2,
         "adjective_3": adjective_3,
@@ -36,16 +40,21 @@ def get_user_input():
     return user_inputs
 
 
-random_story = randint(1, 2)
+random_story = randint(1, 3)
 
 if random_story == 1:
-    print("A Day At The Zoo!")
+    print(f"A Day At The Zoo!")
     file = open("01_mad_libs.txt", "r")
     mad_libs = file.read()
     file.close()
 elif random_story == 2:
-    print("A Day At The Amusement Park!")
+    print(f"A Day At The Amusement Park!")
     file = open("02_mad_libs.txt", "r")
+    mad_libs = file.read()
+    file.close()
+elif random_story == 3:
+    print(f"First Day At School.")
+    file = open("03_mad_libs.txt", "r")
     mad_libs = file.read()
     file.close()
 
